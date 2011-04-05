4ccd297e65534396625e52c8287a7ee607c830c9
Marcello Bastéa-Forte <marcello@cellosoft.com>
2011-04-04 23:15:17 -0400

    refactored + added more tests around count/size

--- test/simple-test.js
+++ test/simple-test.js
@@ -24,41 +24,296 @@ vows.describe('Mongolian DeadBeef, I choose you!').addBatch({
                 assert.equal(names.length,0)
             }
         },
-        "and inserting into 'test',": {
-            topic: function (db) {
-                db.collection('test').insert({ name:'hello world' }, this.callback)
+        "and collection 'test'": {
+            topic: function(db) {
+                return db.collection('test')
             },
-            "it succeeds": function(insertedRow) {
-                assert.equal(insertedRow.name, "hello world")
-            },
-            "has _id": function(insertedRow) {
-                assert.isObject(insertedRow._id)
-            },
-            "we can find it again": {
-                topic: function(insertedRow, db) {
-                    insertedRow.original = "hi mom"
-                    db.collection('test').findOne(this.callback)
+            "and inserting into 'test',": {
+                topic: function (collection,db) {
+                    collection.insert({ name:'hello world' }, this.callback)
                 },
-//                // Wish I knew a clean way to access the insertedRow from the parent scope
-//                "has same _id": function(foundRow) {
-//                    assert.equals(foundRow._id, inserted._id)
-//                },
-                "has _id": function(foundRow) {
-                    assert.isObject(foundRow._id)
+                "it succeeds": function(insertedRow) {
+                    assert.equal(insertedRow.name, "hello world")
                 },
-                "name matches insert": function(foundRow) {
-                    assert.equal(foundRow.name, "hello world")
+                "has _id": function(insertedRow) {
+                    assert.isObject(insertedRow._id)
+                },
+                "we can find it again": {
+                    topic: function(insertedRow, collection, db) {
+                        insertedRow.original = "hi mom"
+                        collection.findOne(this.callback)
+                    },
+    //                // Wish I knew a clean way to access the insertedRow from the parent scope
+    //                "has same _id": function(foundRow) {
+    //                    assert.equals(foundRow._id, inserted._id)
+    //                },
+                    "has _id": function(foundRow) {
+                        assert.isObject(foundRow._id)
+                    },
+                    "name matches insert": function(foundRow) {
+                        assert.equal(foundRow.name, "hello world")
+                    }
+                },
+                "its collectionNames": {
+                    topic: function (insertedRow, collection, db) {
+                        db.collectionNames(this.callback)
+                    },
+                    "should contain 'test'": function(names) {
+                        assert.include(names,"test")
+                    },
+                    "should contain 'system.indexes'": function(names) {
+                        assert.include(names,"system.indexes")
+                    }
                 }
+            }
+        },
+        "and collection 'test2'": {
+            topic: function(db) {
+                return db.collection('test2')
             },
-            "its collectionNames": {
-                topic: function (insertedRow, db) {
-                    db.collectionNames(this.callback)
+            "with 5 inserted documents,": {
+                topic: function (collection, db) {
+                    collection.insert([
+                        { i:1, j:true },
+                        { i:2, j:true },
+                        { i:3, j:false },
+                        { i:4, j:false },
+                        { i:5, j:true }
+                    ], this.callback)
                 },
-                "should contain 'test'": function(names) {
-                    assert.include(names,"test")
+                "it succeeds": function(insertedRows) {
+                    assert.equal(insertedRows.length, 5)
                 },
-                "should contain 'system.indexes'": function(names) {
-                    assert.include(names,"system.indexes")
+                "and searching for one": {
+                    topic: function(insertedRows, collection, db) {
+                        collection.findOne(this.callback)
+                    },
+                    "has i": function(foundRow) {
+                        assert.isNumber(foundRow.i)
+                    }
+                },
+                "and finding all": {
+                    topic: function(insertedRows, collection, db) {
+                        collection.find().toArray(this.callback)
+                    },
+                    "has 5 elements": function(array) {
+                        assert.length(array, 5)
+                    },
+                    "has correct elements": function(array) {
+                        assert.equal(array[0].i,1)
+                        assert.equal(array[1].i,2)
+                        assert.equal(array[2].i,3)
+                        assert.equal(array[3].i,4)
+                        assert.equal(array[4].i,5)
+                    }
+                },
+                "and sorting": {
+                    topic: function(insertedRows, collection, db) {
+                        collection.find().sort({i:1}).toArray(this.callback)
+                    },
+                    "has 5 elements": function(array) {
+                        assert.length(array, 5)
+                    },
+                    "has correct elements": function(array) {
+                        assert.equal(array[0].i,1)
+                        assert.equal(array[1].i,2)
+                        assert.equal(array[2].i,3)
+                        assert.equal(array[3].i,4)
+                        assert.equal(array[4].i,5)
+                    }
+                },
+                "and sorting in reverse": {
+                    topic: function(insertedRows, collection, db) {
+                        collection.find().sort({i:-1}).toArray(this.callback)
+                    },
+                    "has 5 elements": function(array) {
+                        assert.length(array, 5)
+                    },
+                    "has correct elements": function(array) {
+                        assert.equal(array[0].i,5)
+                        assert.equal(array[1].i,4)
+                        assert.equal(array[2].i,3)
+                        assert.equal(array[3].i,2)
+                        assert.equal(array[4].i,1)
+                    }
+                },
+                "and limiting to 3": {
+                    topic: function(insertedRows, collection, db) {
+                        collection.find().limit(3).sort({i:1}).toArray(this.callback)
+                    },
+                    "has 3 elements": function(array) {
+                        assert.length(array, 3)
+                    },
+                    "has correct elements": function(array) {
+                        assert.equal(array[0].i,1)
+                        assert.equal(array[1].i,2)
+                        assert.equal(array[2].i,3)
+                    }
+                },
+                "and skipping 1, limiting to 3": {
+                    topic: function(insertedRows, collection, db) {
+                        collection.find().skip(1).limit(3).sort({i:1}).toArray(this.callback)
+                    },
+                    "has 3 elements": function(array) {
+                        assert.length(array, 3)
+                    },
+                    "has correct elements": function(array) {
+                        assert.equal(array[0].i,2)
+                        assert.equal(array[1].i,3)
+                        assert.equal(array[2].i,4)
+                    }
+                }
+            }
+        },
+        "and collection 'test3'": {
+            topic: function(db) {
+                return db.collection('test3')
+            },
+            "with 1000 inserted documents,": {
+                topic: function (collection, db) {
+                    var array = []
+                    for (var i=0; i<1000; i++) {
+                        array.push({
+                            i:i,
+                            even:(i%2) == 0
+                        })
+                    }
+                    collection.insert(array, this.callback)
+                },
+                "it succeeds": function(err,insertedRows) {
+                    assert.equal(err, null)
+                },
+                "and count": {
+                    topic: function(insertedRows, collection, db) {
+                        collection.find().count(this.callback)
+                    },
+                    "is 1000": function(count) {
+                        assert.equal(count, 1000)
+                    }
+                },
+                "and size": {
+                    topic: function(insertedRows, collection, db) {
+                        collection.find().size(this.callback)
+                    },
+                    "is 1000": function(count) {
+                        assert.equal(count, 1000)
+                    }
+                },
+                "and sorted count": {
+                    topic: function(insertedRows, collection, db) {
+                        collection.find().sort({i:-1}).count(this.callback)
+                    },
+                    "is 1000": function(count) {
+                        assert.equal(count, 1000)
+                    }
+                },
+                "and sorted size": {
+                    topic: function(insertedRows, collection, db) {
+                        collection.find().sort({i:-1}).size(this.callback)
+                    },
+                    "is 1000": function(count) {
+                        assert.equal(count, 1000)
+                    }
+                },
+                "and limit(50) count": {
+                    topic: function(insertedRows, collection, db) {
+                        collection.find().limit(50).count(this.callback)
+                    },
+                    "is 1000": function(count) {
+                        assert.equal(count, 1000)
+                    }
+                },
+                "and limit(50) size": {
+                    topic: function(insertedRows, collection, db) {
+                        collection.find().limit(50).size(this.callback)
+                    },
+                    "is 50": function(count) {
+                        assert.equal(count, 50)
+                    }
+                },
+                "and limit(50).skip(50) count": {
+                    topic: function(insertedRows, collection, db) {
+                        collection.find().limit(50).skip(50).count(this.callback)
+                    },
+                    "is 1000": function(count) {
+                        assert.equal(count, 1000)
+                    }
+                },
+                "and limit(50).skip(50) size": {
+                    topic: function(insertedRows, collection, db) {
+                        collection.find().limit(50).skip(50).size(this.callback)
+                    },
+                    "is 50": function(count) {
+                        assert.equal(count, 50)
+                    }
+                },
+                "and limit(50).skip(990) count": {
+                    topic: function(insertedRows, collection, db) {
+                        collection.find().limit(50).skip(990).count(this.callback)
+                    },
+                    "is 1000": function(count) {
+                        assert.equal(count, 1000)
+                    }
+                },
+                "and limit(50).skip(990) size": {
+                    topic: function(insertedRows, collection, db) {
+                        collection.find().limit(50).skip(990).size(this.callback)
+                    },
+                    "is 10": function(count) {
+                        assert.equal(count, 10)
+                    }
+                },
+                "and sorted": {
+                    topic: function(insertedRows, collection, db) {
+                        collection.find().sort({i:-1}).limit(1000).toArray(this.callback)
+                    },
+                    "length is 1000": function(count) {
+                        assert.length(count, 1000)
+                    }
+                }
+            }
+        },
+        "and collection 'test4'": {
+            topic: function(db) {
+                return db.collection('test4')
+            },
+            "with 10000 inserted documents,": {
+                topic: function (collection, db) {
+                    var array = []
+                    for (var i=0; i<10000; i++) {
+                        array.push({
+                            i:i,
+                            even:(i%2) == 0
+                        })
+                    }
+                    collection.insert(array, this.callback)
+                },
+                "it succeeds": function(err,insertedRows) {
+                    assert.equal(err, null)
+                },
+                "and count": {
+                    topic: function(insertedRows, collection, db) {
+                        collection.find().count(this.callback)
+                    },
+                    "is 10000": function(count) {
+                        assert.equal(count, 10000)
+                    }
+                },
+                "and array": {
+                    topic: function(insertedRows, collection, db) {
+                        collection.find().toArray(this.callback)
+                    },
+                    "length is 10000": function(count) {
+                        assert.length(count, 10000)
+                    }
+                },
+                "and sorted": {
+                    topic: function(insertedRows, collection, db) {
+                        collection.find().sort({i:-1}).limit(10000).toArray(this.callback)
+                    },
+                    "length is 10000": function(count) {
+                        assert.length(count, 10000)
+                    }
                 }
             }
         }
