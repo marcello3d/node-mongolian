91c5016f7d9292816909227b725a2a61498adf57
Marcello Bastea-Forte <marcello@cellosoft.com>
2012-01-31 22:48:22 -0500

    [fix] pass number of updated/deleted rows to update() and remove() methods (fixes GH-84) + tests

--- test/collection1.js
+++ test/collection1.js
@@ -57,8 +57,9 @@ module.exports = {
             test.ifError(error)
             test.ok(insertedRow)
 
-            collection.update({name:'hello sun'}, {age:45}, function(error) {
+            collection.update({name:'hello sun'}, {age:45}, function(error, rowsUpdated) {
                 test.ifError(error)
+                test.equal(rowsUpdated, 1)
 
                 collection.findOne({
                     _id:new Mongolian.ObjectId(insertedRow._id.toString())
@@ -66,6 +67,48 @@ module.exports = {
                     test.ifError(error)
                     test.equal(row.age, 45)
                     test.equal(row.name, undefined)
+                    test.done()
+                })
+            })
+        })
+    },
+    "can insert and remove by _id": function (test) {
+        collection.insert({ name:'hello moon' }, function(error, insertedRow) {
+            test.ifError(error)
+            test.ok(insertedRow)
+
+            collection.remove({name:'hello moon'}, function(error, rowsDeleted) {
+                test.ifError(error)
+                test.equal(rowsDeleted, 1)
+
+                collection.findOne({
+                    _id:new Mongolian.ObjectId(insertedRow._id.toString())
+                }, function(error, row) {
+                    test.ifError(error)
+                    test.equal(row, undefined)
+                    test.done()
+                })
+            })
+        })
+    },
+    "can insert and remove multiple by _id": function (test) {
+        collection.insert([
+                { name:'hello sky' },
+                { name:'hello sky' },
+                { name:'hello sky' },
+                { name:'hello sky' },
+                { name:'hello sky' }
+            ], function(error, insertedRow) {
+            test.ifError(error)
+            test.ok(insertedRow)
+
+            collection.remove({name:'hello sky'}, function(error, rowsDeleted) {
+                test.ifError(error)
+                test.equal(rowsDeleted, 5)
+
+                collection.find({name:'hello sky'}).count(function(error, count) {
+                    test.ifError(error)
+                    test.equal(count, 0)
                     test.done()
                 })
             })
@@ -97,6 +140,19 @@ module.exports = {
                     test.equal(foundRow.name, "awesome hot sauce")
                     test.done()
                 })
+            })
+        })
+    },
+    "saving with bad id": function(test) {
+        var id = new Mongolian.ObjectId("AAAAAAAAAAAAAAAAAAAAAAAA")
+        collection.save({ _id:id, name:'hello there' }, function(error, insertedRow) {
+            test.ifError(error)
+            test.equal(insertedRow.name, "hello there")
+            test.equal(insertedRow._id.toString(), id.toString())
+            collection.findOne({_id:insertedRow._id}, function(error, foundRow) {
+                test.equal(foundRow.name, "hello there")
+                test.equal(foundRow._id.toString(), id.toString())
+                test.done()
             })
         })
     },
